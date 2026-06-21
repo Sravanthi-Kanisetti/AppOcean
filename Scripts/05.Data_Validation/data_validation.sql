09f29af95ec2be7a229710cd49ac9d4d745f8f91
Sravanthi-Kanisetti <kanisettisravanthi@gmail.com>
2026-06-21 08:33:32 +0530

    Update data_validation.sql

--- Scripts/05.Data_Validation/data_validation.sql
+++ Scripts/05.Data_Validation/data_validation.sql
@@ -33,7 +33,7 @@ select 'dim_store_region' as table_name,
 union all
 select 'fact_app_events' as table_name,
 (select count(*) from Staging.fact_app_events)  as Staging_rows,
-(select count(*) from DW.fact_app_events)as DW_rows
+(select count(*) from DW.fact_app_events)as DW_rows;
 
 
 
@@ -44,7 +44,7 @@ from staging.fact_app_events
 union all
 select 
 distinct event_type collate latin1_general_CS_AS,'DW' as source
-from DW.fact_app_events
+from DW.fact_app_events;
 
 
 select 
@@ -53,7 +53,7 @@ from staging.fact_app_events
 union all
 select 
 count(distinct event_type collate latin1_general_CS_AS),'DW' as source,'event_type'
-from DW.fact_app_events
+from DW.fact_app_events;
 
 
 
@@ -62,7 +62,7 @@ select count(*)-count(install_source) as nulls_count ,'Staging' as source ,'inst
 from Staging.fact_app_events 
 union all
 select count(*)-count(install_source) as nulls_count ,'DW' as source ,'install_source' as column_name
-from DW.fact_app_events 
+from DW.fact_app_events;
 
 
 select 
@@ -71,7 +71,7 @@ from staging.fact_app_events
 union all
 select 
 count(distinct install_source collate latin1_general_CS_AS),'DW' as source,'install_source'
-from DW.fact_app_events
+from DW.fact_app_events;
 
 
 
@@ -81,7 +81,7 @@ from staging.fact_app_events
 union all
 select 
 distinct install_source collate latin1_general_CS_AS,'DW' as source
-from DW.fact_app_events
+from DW.fact_app_events;
 
 select 'Staging' as source,
 max(discount_pct) as max_discount_pct,
@@ -93,7 +93,7 @@ union all
 select 'DW' as source,max(discount_pct) as max_discount_pct,min(discount_pct) as min_discount_pct,
 sum(case when discount_pct<0 then 1 else 0 end),
 count(*)-count(discount_pct)
-from DW.fact_app_events
+from DW.fact_app_events;
 
 
 
@@ -101,7 +101,7 @@ select 'Staging' as source,count(*)-count(app_version)  as nulls_count
 from Staging.fact_app_events
 union all 
 select 'DW' as source,count(*)-count(app_version)
-from DW.fact_app_events
+from DW.fact_app_events;
 
 
 
@@ -110,14 +110,14 @@ select distinct platform  collate latin1_general_CS_AS as platform,'Staging' as 
 from Staging.dim_app
 union all
 select distinct platform collate latin1_general_CS_AS,'DW' as source
-from DW.dim_app
+from DW.dim_app;
 
 
 select count(*)-count(content_rating) as nulls_count ,'Staging' as source,'content_rating' as column_name
 from Staging.dim_app
 union all 
 select count(*)-count(content_rating) as nulls_count ,'DW' as source,'content_rating'
-from DW.dim_app
+from DW.dim_app;
 
 
 
@@ -126,7 +126,7 @@ select distinct is_weekend ,'Staging' as source
 from Staging.dim_date 
 union all
 select distinct cast( is_weekend as nvarchar),'DW'
-from DW.dim_date 
+from DW.dim_date ;
 
 
 
@@ -134,32 +134,32 @@ select distinct device_type collate latin1_general_CS_AS as device_type ,'Stagin
 from Staging.dim_device 
 union all
 select distinct device_type collate latin1_general_CS_AS ,'DW' as source
-from DW.dim_device 
+from DW.dim_device ;
 
 select count(distinct device_type collate latin1_general_CS_AS) as unique_values_count,'Staging' as source ,'device_type' as column_name
 from Staging.dim_device 
 union all
 select count(distinct device_type collate latin1_general_CS_AS),'DW' as source ,'device_type' as column_name
-from DW.dim_device 
+from DW.dim_device ;
 
 
 select count(*)-count(device_type) as nulls_count ,'Staging' as source ,'device_type' as column_name
 from Staging.dim_device 
 union all
 select count(*)-count(device_type) as nulls_count ,'DW' as source ,'device_type' as column_name
-from DW.dim_device 
+from DW.dim_device ;
 
 select count(*)-count(os_version) as nulls_count ,'Staging' as source ,'os_version' as column_name
 from Staging.dim_device 
 union all
 select count(*)-count(os_version) as nulls_count ,'DW' as source ,'os_version' as column_name
-from DW.dim_device 
+from DW.dim_device ;
 
 select count(*)-count(manufacturer) as nulls_count ,'Staging' as source ,'manufacturer' as column_name
 from Staging.dim_device 
 union all
 select count(*)-count(manufacturer) as nulls_count ,'DW' as source ,'manufacturer' as column_name
-from DW.dim_device 
+from DW.dim_device ;
 
  
 
@@ -169,20 +169,20 @@ select distinct gender collate latin1_general_CS_AS as gender ,'Staging' as sour
 from Staging.dim_user 
 union all
 select distinct gender collate latin1_general_CS_AS ,'DW' as source
-from DW.dim_user 
+from DW.dim_user ;
 
 select count(distinct gender collate latin1_general_CS_AS) as unique_values_count,'Staging' as source ,'gender' as column_name
 from Staging.dim_user 
 union all
 select count(distinct gender collate latin1_general_CS_AS),'DW' as source ,'gender' as column_name
-from DW.dim_user 
+from DW.dim_user ;
 
 
 select count(*)-count(gender) as nulls_count ,'Staging' as source ,'gender' as column_name
 from Staging.dim_user 
 union all
 select count(*)-count(gender) as nulls_count ,'DW' as source ,'gender' as column_name
-from DW.dim_user 
+from DW.dim_user ;
 
 
 
@@ -191,20 +191,20 @@ select distinct age_group collate latin1_general_CS_AS as age_group ,'Staging' a
 from Staging.dim_user 
 union all
 select distinct age_group collate latin1_general_CS_AS ,'DW' as source
-from DW.dim_user 
+from DW.dim_user ;
 
 select count(distinct age_group collate latin1_general_CS_AS) as unique_values_count,'Staging' as source ,'age_group' as column_name
 from Staging.dim_user 
 union all
 select count(distinct age_group collate latin1_general_CS_AS),'DW' as source ,'age_group' as column_name
-from DW.dim_user 
+from DW.dim_user; 
 
 
 select count(*)-count(age_group) as nulls_count ,'Staging' as source ,'age_group' as column_name
 from Staging.dim_user 
 union all
 select count(*)-count(age_group) as nulls_count ,'DW' as source ,'age_group' as column_name
-from DW.dim_user 
+from DW.dim_user; 
 
 
 
@@ -212,14 +212,14 @@ select count(*)-count(city) as nulls_count ,'Staging' as source ,'city' as colum
 from Staging.dim_user 
 union all
 select count(*)-count(city) as nulls_count ,'DW' as source ,'city' as column_name
-from DW.dim_user 
+from DW.dim_user; 
 
 
 select count(*)-count(email_domain) as nulls_count ,'Staging' as source ,'email_domain' as column_name
 from Staging.dim_user 
 union all
 select count(*)-count(email_domain) as nulls_count ,'DW' as source ,'email_domain' as column_name
-from DW.dim_user 
+from DW.dim_user ;
 
 
 
@@ -227,13 +227,13 @@ select distinct is_premium collate latin1_general_CS_AS as is_premium ,'Staging'
 from Staging.dim_user 
 union all
 select distinct cast( is_premium   as nvarchar),'DW' as source
-from DW.dim_user 
+from DW.dim_user ;
 
 select count(distinct is_premium collate latin1_general_CS_AS) as unique_values_count,'Staging' as source ,'is_premium' as column_name
 from Staging.dim_user 
 union all
 select count(distinct is_premium ),'DW' as source ,'is_premium' as column_name
-from DW.dim_user 
+from DW.dim_user ;
 
 
 
